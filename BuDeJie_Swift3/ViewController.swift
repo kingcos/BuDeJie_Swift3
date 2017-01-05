d0054d7b0440adcce15087920998210f8ddd9fe9
kingcos <2821836721v@gmail.com>
2017-01-06 00:46:01 +0800

    Create a new project

---
+++ BuDeJie_Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuDeJie_Swift3
+//
+//  Created by 买明 on 06/01/2017.
+//  Copyright © 2017 买明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
